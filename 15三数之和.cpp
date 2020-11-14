c4ef89a9194fabf8c6cfe1af4544244cfc3d3bf8
fshi2006 <50450759+fshi2006@users.noreply.github.com>
2020-11-14 11:16:44 +0800

    2020/11/14

---
+++ 15三数之和.cpp
@@ -0,0 +1,40 @@
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
+        int left=0,right=nums.size()-1;
+        for(int i = 0;i < nums.size(); i++)
+        {
+            if(nums[i] > 0)
+            {
+                return result;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            left = i+1;right=nums.size()-1;
+            while(left<right)
+            {
+                if(nums[i]+nums[left]+nums[right] < 0)
+                {
+                    left++;
+                }
+                else if(nums[i]+nums[left]+nums[right] > 0)
+                {
+                    right--;
+                }
+                else if(nums[i]+nums[left]+nums[right]==0)
+                {
+                    result.push_back(vector<int>{nums[i], nums[left], nums[right]});
+                    while(right>left&&nums[left]==nums[left+1]) left++;
+                    while(right>left&&nums[right]==nums[right-1]) right--;                  
+                    right--;
+                    left++;
+                }
+            }   
+        }
+        return result;
+    }
+};
